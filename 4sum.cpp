3ed1f3f0aaed82a8dbb7e74cbf12bbcc6ff6f386
Harshi <94774731+harshi-s@users.noreply.github.com>
2023-03-07 22:20:18 +0530

    Create 4sum.cpp

---
+++ 4sum.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n=nums.size();
+        sort(nums.begin(), nums.end());
+        set<vector<int>> s;
+        vector<vector<int>> ans;
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                int k=j+1;
+                int e=n-1;
+                while(k<e){
+                long long int sum=(long long)nums[i]+(long long)nums[j]+(long long)nums[k]
+                +(long long)   nums[e];
+
+                    if(sum==target) {
+                        s.insert({nums[i],nums[j],nums[k],nums[e]});
+                        k++;
+                        e--;
+                    }
+                    else if(sum < target) k++;
+                    else e--;
+                }
+                   
+            }
+        }
+        for(auto i: s){
+            ans.push_back(i);
+        }
+        return ans;
+    }
+};
